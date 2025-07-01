26c6fe619e0a23bf62a1835e145270b7206b86d1
simplicio10 <claypas@gmail.com>
2025-07-01 11:05:59 -0500

    Added fourth prompt for testing

--- prompt-testing/prompts/prompts.py
+++ prompt-testing/prompts/prompts.py
@@ -44,3 +44,25 @@ def better_initial_prompt_2(brief):
     All cases should go in <case> tags, all statutes in <statute> tags, and all court rules in <rule> tags.
     Finally, provide the total number of cases in <case_count> tags. Please include only a number inside the <case_count> tags.
     """
+
+def refactored_prompt(brief):
+    #Tries to refactor Prompt 2 and 3
+    return f"""
+    Included below is a legal brief:
+    <brief>
+    {brief}
+    </brief>
+
+    Please carefully review the brief and provide me a list of every case that is cited in the brief.
+
+    When a case is cited, you should not list any quoted cases in parentheses. 
+    <example>
+    For example, if the citation is 
+    "Zadvydas v. Davis, 533 U.S. 678, 719 (2001) (citing Shaughnessy v. United States ex rel. Mezei, 345 U.S. 206 (1953)),"
+    you should only list Zadvydas v. Davis, 533 U.S. 678, 719 (2001).
+    </example>
+
+    Think step by step. As you think, put your thoughts in <thinking> tags and your final list of cases in <final_list> tags.
+    All cases should go in <case> tags.
+    Finally, provide the total number of cases in <case_count> tags. Please include only a number inside the <case_count> tags.
+    """
